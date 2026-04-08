c482f7cfab4bb6ec57904ef6c914ab3829c0fda3
sergei_romanov <sergemrcat@gmail.com>
2026-04-08 18:20:10 +0300

    add movie search logic

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,33 @@
 import SearchBar from "../SearchBar/SearchBar";
+import toast, { Toaster } from "react-hot-toast";
+import { useState } from "react";
+import { fetchMovies } from "../../services/movieService";
+import type { Movie } from "../../types/movie";
 
 export default function App() {
-  const handleSearch = (query: string) => {
-    console.log("Search", query);
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const handleSearch = async (query: string) => {
+    try {
+      setMovies([]);
+
+      const data = await fetchMovies(query);
+
+      if (data.length === 0) {
+        toast.error("No movies found for your request.");
+        return;
+      }
+
+      setMovies(data);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  return <SearchBar onSubmit={handleSearch} />;
+  return (
+    <>
+      <Toaster />
+      <SearchBar onSubmit={handleSearch} />
+    </>
+  );
 }
--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,3 +1,4 @@
+import toast from "react-hot-toast";
 import styles from "./SearchBar.module.css";
 
 interface SearchBarProps {
@@ -9,7 +10,7 @@ export default function SearchBar({ onSubmit }: SearchBarProps) {
     const query = formData.get("query") as string;
 
     if (!query.trim()) {
-      alert("Please enter your search query.");
+      toast.error("Please enter your search query.");
       return;
     }
 
